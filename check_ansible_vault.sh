92f6904ec7b59bca25733c6650b099aa4ea12f1d
thecomalley <31399219+thecomalley@users.noreply.github.com>
2024-08-20 22:03:45 +1200

    precommit

---
+++ check_ansible_vault.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Check if any files don't have the Ansible Vault header
+
+for file in "$@"; do
+    if grep -q '^$ANSIBLE_VAULT' "$file"; then
+        echo "Ansible Vault encryption detected in $file"
+    else
+        echo "Error: $file is not encrypted with Ansible Vault."
+        exit 1
+    fi
+done
